a9fd31c157bc67ebea7a6d72a86d20546da2b4ad
mich0w0h <aokimiho71@gmail.com>
2024-01-09 16:06:24 +0900

    refactor: simplify the codes calling blinkWithTimer() only once

--- frontend/src/components/CharacterDisplay.tsx
+++ frontend/src/components/CharacterDisplay.tsx
@@ -16,34 +16,32 @@ const CharacterDisplay: React.FC = () => {
         }
     }
     
-    const timeoutIdRef = useRef<number | null>(null); // for clearTimeout
-    
     const blinkWithTimer = async (timer: number): Promise<void> => {
-        console.log('called blinkWithTimer');
+        console.log('start blinkWithTimer: ' + timer);
         const interval = 1500;
         await blinkEyes(1); // One blink
         await delay(interval); // Delay after one blink
         await blinkEyes(2); // Two blinks
         await delay(interval); // Delay after two blinks
         await blinkEyes(1); // One final blink
-        timeoutIdRef.current = setTimeout(() => {
+        
+        console.log('set next blinkWithTimer');
+        setTimeout(() => {
             blinkWithTimer(timer);
-        }, timer) as number;
-        console.log('create timeout:' + timeoutIdRef.current);
+        }, timer);
     }
+
 
     const hasMounted = useRef<boolean>(false);
     useEffect(() => {
         if(!hasMounted.current) {
-            hasMounted.current =true;
+            hasMounted.current = true;
+            console.log("initialize");
             blinkWithTimer(3000);
         }
     
         return () => {
-            if (timeoutIdRef.current) {
-                console.log('clear timeout:' + timeoutIdRef.current);
-                clearTimeout(timeoutIdRef.current);
-            }
+            // called when the component is unmounted
         };
     }, []);
     
