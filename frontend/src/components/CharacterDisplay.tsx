1d158b6ce28fc181feb9dea71c70ce7a8e02a554
mich0w0h <aokimiho71@gmail.com>
2024-02-15 09:51:55 +0900

    chore: ran prettier formatter

--- frontend/src/components/CharacterDisplay.tsx
+++ frontend/src/components/CharacterDisplay.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import CharacterEar from './CharacterEar';
-import CharacterFace from './CharacterFace';
-import './CharacterDisplay.css';
+import React from "react";
+import CharacterEar from "./CharacterEar";
+import CharacterFace from "./CharacterFace";
+import "./CharacterDisplay.css";
 
 const CharacterDisplay: React.FC = () => {
-    const delay = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
+  const delay = async (ms: number): Promise<void> => {
+    await new Promise<void>((resolve) => setTimeout(resolve, ms));
+  };
 
-    return (
-        <div className="character">
-            <img src="/src/assets/character-body.png" alt="Character body" className="character-body" />
+  return (
+    <div className="character">
+      <img
+        src="/src/assets/character-body.png"
+        alt="Character body"
+        className="character-body"
+      />
 
-            <CharacterEar side="left" delayFunction={delay} />
-            <CharacterEar side="right" delayFunction={delay} />
+      <CharacterEar side="left" delayFunction={delay} />
+      <CharacterEar side="right" delayFunction={delay} />
 
-            <CharacterFace delayFunction={delay} />
-        </div>
-    );
+      <CharacterFace delayFunction={delay} />
+    </div>
+  );
 };
 
 export default CharacterDisplay;
